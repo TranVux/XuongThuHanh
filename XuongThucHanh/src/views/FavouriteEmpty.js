f767355af20110bc032fd7544d486ae1f96bf010
Nguyễn Bảo Ngọc <ngbaongocit.4503@gmail.com>
2023-02-20 19:56:35 +0700

    views

---
+++ XuongThucHanh/src/views/FavouriteEmpty.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View, Image, Button, Pressable, TextInput } from 'react-native'
+import React from 'react'
+
+const FavouriteEmpty = () => {
+    return (
+        <View style={styles.container}>
+            <Image style={styles.back} source={require('../img/back.png')} />
+            <Text style={styles.sign}>Sign In</Text>
+            <Image style={styles.login} source={require('../img/login.png')} />
+            <Text style={styles.guide}>Enter your phone number and {'\n'}password to access your account</Text>
+        </View>
+    )
+}
+
+export default FavouriteEmpty
+
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 16
+    },
+    back: {
+        marginStart: 4,
+        marginTop: 10.5
+    },
+    sign: {
+        textAlign: 'center',
+        fontFamily: 'Klarna Text',
+        fontSize: 24,
+        fontWeight: '700',
+        color: '#FF5E00'
+    },
+    login: {
+        width: 347.28,
+        height: 331.24
+    },
+    guide: {
+        fontFamily: 'Klarna Text',
+        fontSize: 18,
+        fontWeight: '400',
+        color: '#6D3805'
+    },
+})
