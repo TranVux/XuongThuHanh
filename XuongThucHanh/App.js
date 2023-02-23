b0e4e110ec96072f8dd948771a057221e004b7ec
lhhdai06022003 <lhhd06022003@gmail.com>
2023-02-23 09:18:20 +0700

    manhinh
    cập nhật màn hình order,profile,edit profile,change pass

--- XuongThucHanh/App.js
+++ XuongThucHanh/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { View } from 'react-native'
 import { AppContextProvider } from './src/utils/AppContext'
+import ChangePass from './src/Views/ChangePass'
+import EditProfile from './src/Views/EditProfile'
+import Orders from './src/Views/Orders'
+import Profile from './src/Views/Profile'
+import ToggleButton from './src/Views/ToggleButton'
 
 const App = () => {
   return (
-    <AppContextProvider>
-      {/* AppNavigation */}
-    </AppContextProvider>
+    <View>
+      <ChangePass />
+    </View>
   )
 }
 
