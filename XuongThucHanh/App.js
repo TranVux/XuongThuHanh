5eb738e02773d679e44d61808d7d09ba81f8805e
mujga <99776573+mujga@users.noreply.github.com>
2023-02-26 02:40:54 +0700

    update_giaodien_mycard
    trien khai man hinh my card va new my card

--- XuongThucHanh/App.js
+++ XuongThucHanh/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { View } from 'react-native'
 import { AppContextProvider } from './src/utils/AppContext'
+import MyCard from './src/Screen/card/MyCard'
 
+import NewCard from './src/Screen/card/NewCard'
 const App = () => {
   return (
-    <AppContextProvider>
-      {/* AppNavigation */}
-    </AppContextProvider>
+    // <AppContextProvider>
+    //   {/* AppNavigation */}
+    // </AppContextProvider>
+
+    <NewCard />
+
+
   )
 }
 
