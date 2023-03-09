783a7e71b83bf1215006dee584d3d0b28e104995
Nguyễn Bảo Ngọc <ngbaongocit.4503@gmail.com>
2023-03-09 22:56:31 +0700

    format code

--- XuongThucHanh/App.js
+++ XuongThucHanh/App.js
@@ -5,19 +5,20 @@ import { Provider } from 'react-redux'
 import store from './src/redux/store'
 
 import { enableFreeze } from 'react-native-screens';
+import { FavouriteEmpty } from './src/screens'
 enableFreeze(true);
 
 const App = () => {
    return (
-      <View>
-         <Mango />
-         <Favourite />
+      // <View>
+      //    {/* <Mango /> */}
+      //    <FavouriteEmpty/>
 
-      </View>
-      // <Provider store={store}>
-      //   <StatusBar backgroundColor={"#fff"} barStyle={'dark-content'} />
-      //   <AppNavigator />
-      // </Provider>
+      // </View>
+      <Provider store={store}>
+        <StatusBar backgroundColor={"#fff"} barStyle={'dark-content'} />
+        <AppNavigator />
+      </Provider>
    )
 }
 
