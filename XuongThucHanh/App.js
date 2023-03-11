4d30fda8f09c0c6d8742b2f0dd826db6d3196eec
TranVux <vutran789jjjj@gmail.com>
2023-03-11 13:56:38 +0700

    Update icon Profile screen

--- XuongThucHanh/App.js
+++ XuongThucHanh/App.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { StatusBar } from 'react-native'
-import AppNavigator from './src/routes/AppNavigator'
-import { Provider } from 'react-redux'
-import store from './src/redux/store'
+import React from 'react';
+import {StatusBar} from 'react-native';
+import AppNavigator from './src/routes/AppNavigator';
+import {Provider} from 'react-redux';
+import store from './src/redux/store';
 
-import { enableFreeze } from 'react-native-screens';
+import {enableFreeze} from 'react-native-screens';
 enableFreeze(true);
 
 const App = () => {
   return (
     <Provider store={store}>
-      <StatusBar backgroundColor={"#fff"} barStyle={'dark-content'} />
+      <StatusBar backgroundColor={'#fff'} barStyle={'dark-content'} />
       <AppNavigator />
     </Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- XuongThucHanh/src/screens/Profile.js
+++ XuongThucHanh/src/screens/Profile.js
@@ -1,12 +1,24 @@
-import { StyleSheet, Text, View, Image, Switch, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Switch,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
 import FastImage from 'react-native-fast-image';
 
 const Profile = () => {
   const [isEnabled, setIsEnabled] = useState(false);
   return (
     <View style={styles.profile}>
-      <TouchableOpacity><FastImage style={styles.Arrow_ic} source={require('../../assets/images/Arrow-ic.png')}></FastImage></TouchableOpacity>
+      <TouchableOpacity>
+        <FastImage
+          resizeMode={FastImage.resizeMode.contain}
+          style={styles.Arrow_ic}
+          source={require('../../assets/images/Arrow-ic.png')}></FastImage>
+      </TouchableOpacity>
       <View style={styles.title}>
         <Text style={styles.title_text}>Profile</Text>
       </View>
@@ -15,30 +27,47 @@ const Profile = () => {
       <View style={styles.account}>
         {/* edit profile */}
         <View style={styles.account_edit}>
-          <FastImage style={styles.account_img} source={require('../../assets/images/user.png')}></FastImage>
+          <FastImage
+            resizeMode={FastImage.resizeMode.contain}
+            style={styles.account_img}
+            source={require('../../assets/images/user.png')}></FastImage>
           <View style={styles.account_title_arrow}>
             <Text style={styles.account_title}>Edit Profile</Text>
-            <FastImage style={styles.account_arrow} source={require('../../assets/images/back_arrow.png')}></FastImage>
+            <FastImage
+              resizeMode={FastImage.resizeMode.contain}
+              style={styles.account_arrow}
+              source={require('../../assets/images/back_arrow.png')}></FastImage>
           </View>
         </View>
         {/* change pass */}
         <View style={styles.account_edit}>
-          <FastImage style={styles.account_img} source={require('../../assets/images/key.png')}></FastImage>
+          <FastImage
+            resizeMode={FastImage.resizeMode.contain}
+            style={styles.account_img}
+            source={require('../../assets/images/key.png')}></FastImage>
           <View style={styles.account_title_arrow}>
             <Text style={styles.account_title}>Change Password</Text>
-            <FastImage style={styles.account_arrow} source={require('../../assets/images/back_arrow.png')}></FastImage>
+            <FastImage
+              resizeMode={FastImage.resizeMode.contain}
+              style={styles.account_arrow}
+              source={require('../../assets/images/back_arrow.png')}></FastImage>
           </View>
         </View>
 
         {/* my cards */}
-        <View style={[styles.account_edit, { marginBottom: 0 }]}>
-          <FastImage style={styles.account_img} source={require('../../assets/images/cart.png')}></FastImage>
+        <View style={[styles.account_edit, {marginBottom: 0}]}>
+          <FastImage
+            resizeMode={FastImage.resizeMode.contain}
+            style={styles.account_img}
+            source={require('../../assets/images/cart.png')}></FastImage>
           <View style={styles.account_title_arrow}>
             <Text style={styles.account_title}>Edit Profile</Text>
-            <FastImage style={styles.account_arrow} source={require('../../assets/images/back_arrow.png')}></FastImage>
+            <FastImage
+              resizeMode={FastImage.resizeMode.contain}
+              style={styles.account_arrow}
+              source={require('../../assets/images/back_arrow.png')}></FastImage>
           </View>
         </View>
-
       </View>
 
       {/* App Setting */}
@@ -47,12 +76,15 @@ const Profile = () => {
 
         {/* Notifications */}
         <View style={[styles.account_edit]}>
-          <FastImage style={styles.account_img} source={require('../../assets/images/bell.png')}></FastImage>
+          <FastImage
+            resizeMode={FastImage.resizeMode.contain}
+            style={styles.account_img}
+            source={require('../../assets/images/bell.png')}></FastImage>
           <View style={styles.account_title_arrow}>
             <Text style={styles.account_title}>Notifications</Text>
             <View style={styles.togglebutton}>
               <Switch
-                trackColor={{ false: '#767577', true: '#FF5E00' }}
+                trackColor={{false: '#767577', true: '#FF5E00'}}
                 thumbColor={isEnabled ? '#FFFFFF' : '#FFFFFF'}
                 onValueChange={() => setIsEnabled(!isEnabled)}
                 value={isEnabled}
@@ -62,50 +94,61 @@ const Profile = () => {
         </View>
 
         {/* language */}
-        <View style={[styles.appsettings, { marginBottom: 33 }]}>
+        <View style={[styles.appsettings, {marginBottom: 33}]}>
           <View style={styles.appsettings_left}>
-            <FastImage style={styles.account_img} source={require('../../assets/images/language.png')}></FastImage>
-            <Text style={[styles.account_title, { marginLeft: 15 }]}>Languages</Text>
+            <FastImage
+              resizeMode={FastImage.resizeMode.contain}
+              style={styles.account_img}
+              source={require('../../assets/images/language.png')}></FastImage>
+            <Text style={[styles.account_title, {marginLeft: 15}]}>
+              Languages
+            </Text>
           </View>
           <View style={styles.appsettings_right}>
             <Text style={styles.account_select}>English</Text>
-            <FastImage style={styles.account_arrow} source={require('../../assets/images/back_arrow.png')}></FastImage>
+            <FastImage
+              resizeMode={FastImage.resizeMode.contain}
+              style={styles.account_arrow}
+              source={require('../../assets/images/back_arrow.png')}></FastImage>
           </View>
         </View>
 
         {/* logout */}
         <View style={[styles.account_edit]}>
-          <FastImage style={styles.account_img} source={require('../../assets/images/logout.png')}></FastImage>
-          <Text style={[styles.account_title, { marginLeft: 15 }]}>Logout</Text>
+          <FastImage
+            resizeMode={FastImage.resizeMode.contain}
+            style={styles.account_img}
+            source={require('../../assets/images/logout.png')}></FastImage>
+          <Text style={[styles.account_title, {marginLeft: 15}]}>Logout</Text>
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
 
 const styles = StyleSheet.create({
   profile: {
     flex: 1,
     paddingTop: 10,
     paddingHorizontal: 20,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
   },
   Arrow_ic: {
     width: 8.49,
-    height: 14
+    height: 14,
   },
   title: {
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   title_text: {
     fontFamily: 'Klarna Text',
     fontWeight: '700',
     fontSize: 24,
     lineHeight: 28.8,
-    color: '#FF5E00'
+    color: '#FF5E00',
   },
   account: {
     height: 137,
@@ -118,11 +161,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 33,
-    justifyContent: 'flex-start'
+    justifyContent: 'flex-start',
   },
   account_img: {
-    width: 20,
-    height: 24
+    width: 24,
+    height: 24,
   },
   account_title_arrow: {
     flex: 1,
@@ -130,14 +173,14 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginLeft: 15
+    marginLeft: 15,
   },
   account_title: {
     fontFamily: 'Klarna Text',
     fontWeight: '700',
     fontSize: 18,
     lineHeight: 21.6,
-    color: '#804F1E'
+    color: '#804F1E',
   },
   account_arrow: {
     width: 6.67,
@@ -155,7 +198,7 @@ const styles = StyleSheet.create({
     fontSize: 22,
     lineHeight: 26.4,
     color: '#FF5E00',
-    marginBottom: 32
+    marginBottom: 32,
   },
   togglebutton: {
     alignItems: 'center',
@@ -165,17 +208,17 @@ const styles = StyleSheet.create({
     height: 24,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   appsettings_left: {
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   appsettings_right: {
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   account_select: {
     marginRight: 14,
@@ -183,6 +226,6 @@ const styles = StyleSheet.create({
     fontWeight: '400',
     fontSize: 16,
     lineHeight: 19.2,
-    color: '#804F1E'
-  }
-})
+    color: '#804F1E',
+  },
+});
